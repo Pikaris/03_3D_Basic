70fae43c852b7211e50740d857904f9b14f4a649
tesia102 <koworihime@gmail.com>
2024-09-09 14:06:48 +0900

    UIUX

--- Assets/Scripts/Common/IUnlockable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// 열쇠로 잠금
-public interface IUnlockable
-{
-    void UnLock();
-}
--- Assets/Scripts/Player/IPlatformRide.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IPlatformRide
-{
-    public void OnRidePlatform(Vector3 moveDelta);
-}
